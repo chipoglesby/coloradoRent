ee8630a343b75a0446da289c77153a267b39849d
Chip Oglesby <chip@creativesparkcolumbia.com>
2017-04-21 19:01:24 -0600

    Update load.R

--- code/load.R
+++ code/load.R
@@ -25,4 +25,7 @@ if(!file.exists("data/coloradoRent.csv")){
   coloradoRent <- read_csv("data/coloradoRent.csv")  
   coloradoRent %<>% 
     na.omit()
+  coloradoRent$averagerent <- as.numeric(coloradoRent$averagerent)
+  coloradoRent$year <- as.integer(coloradoRent$year)
+  coloradoRent$quarter <- as.integer(coloradoRent$quarter)
 }
